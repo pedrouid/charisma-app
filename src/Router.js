73dcd4055ba46e82aa620c8193af99bf90aeb3a5
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-03 23:13:37 +0100

    linting errors

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import GradientBackground from './components/GradientBackground';
 import Column from './components/Column';
 import HomePage from './pages/HomePage';
