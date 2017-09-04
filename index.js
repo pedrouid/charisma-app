8e530c29c1a8d7b326300f812fb90073881a4517
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-04 14:12:02 +0100

    middleware serve static

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server listening on ${port}`));
+app.listen(port, () => console.log(`Server lisstening on ${port}`));
